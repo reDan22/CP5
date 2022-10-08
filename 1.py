802b60e69d605f42975f4698a9d6f024505bccb1
unknown <daniilfrolov5@gmail.com>
2022-10-08 17:09:01 +0300

    Load

---
+++ 1.py
@@ -0,0 +1,20 @@
+import random
+
+print("Введите число DELTA:")
+try:
+    count = 0
+    delta = int(input())
+    array = [0] * 10 
+    for i in range(len(array)):
+        array[i] = random.randint(0,99)
+    print(array)
+    minimum = min(array)
+
+    for j in array:
+        if j - minimum == delta:
+            count+=1
+
+    print("Общее число элементов в массиве, отличающихся от минимального на DELTA = " + str(count))
+except ValueError:
+    print("Введите ЧИСЛО")
+    quit()
